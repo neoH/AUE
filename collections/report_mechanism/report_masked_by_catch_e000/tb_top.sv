a6977cd91aab63af356641d4079282e1b554d478
huangqi <huangqi@localhost.localdomain>
2020-04-23 09:04:08 -0400

    e000 published

--- collections/report_mechanism/report_masked_by_catch_e000/tb_top.sv
+++ collections/report_mechanism/report_masked_by_catch_e000/tb_top.sv
@@ -15,12 +15,24 @@
 
 module tb_top; // {
 
+	report_catcher rc = new("rc");
 	import uvm_pkg::*;
 
 	initial begin // {
 		#1us;
 		$finish;
 	end // }
+
+
+	initial begin // {
+		uvm_report_cb::add(null,rc);
+		`uvm_info("TESTID","reported this test ID",UVM_LOW)
+		rc.add_mask("TESTID");
+		`uvm_info("TESTID2","ID: TESTID masked",UVM_LOW)
+		`uvm_info("TESTID","reported this test ID, if printed, then failed",UVM_LOW)
+	end // }
+
+
 
 
 endmodule // }
